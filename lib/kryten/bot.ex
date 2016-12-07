a6f0b275dcac809cd839f37ee3e1685b7f927ee6
Ben Hallett <ben.hallett@jobseeker.com.au>
2016-12-07 12:34:44 +1100

    Jenkins building

--- lib/kryten/bot.ex
+++ lib/kryten/bot.ex
@@ -1,21 +1,22 @@
 defmodule Kryten.Bot do
   use Slack
+  alias Kryten.Jenkins
 
   def start_link(token) do
     Slack.Bot.start_link(__MODULE__, [], token)
   end
 
-  def handle_connect(slack, state) do
+  def handle_connect(_slack, state) do
     {:ok, state}
   end
 
-  def handle_event(message = %{type: "message"}, slack, state) do
-    IO.inspect message
-    matches = Regex.named_captures ~r/<@#{slack.me.id}>:?\sdeploy\s(?<project>\w+)/, message.text
+  def handle_event(message = %{type: "message", text: message_text}, slack, state) do
+    matches = Regex.named_captures ~r/<@#{slack.me.id}>:?\sdeploy\s(?<project>\w+)/, message_text
 
     if matches do
-      send_message("<@#{message.user}> deploying #{Map.get(matches, "project")}", message.channel, slack)
-
+      project = Map.get(matches, "project")
+      send_message("<@#{message.user}> deploying #{project}", message.channel, slack)
+      Jenkins.deploy project
     end
 
     {:ok, state}
--- lib/kryten/jenkins.ex
+++ lib/kryten/jenkins.ex
@@ -3,20 +3,17 @@ defmodule Kryten.Jenkins do
 
   ## API
 
-  def build(project) do
-    post!(project, "", %{}, auth)
+  def deploy(project) do
+    post!(project, "", %{"Content-Type" => "application/x-www-form-urlencoded"}, auth)
   end
 
   ## CALLBACKS
 
   def process_url(project) do
-    #"https://cicd.jora.com/job/#{project}/build?token=#{jenkins_token}" |> IO.inspect
-    "http://localhost:8081/#{project}?token=#{jenkins_token}"
+    "https://cicd.jora.com/job/#{project}/build"
   end
 
   def auth, do: [hackney: [insecure: true, basic_auth: {github_user, github_token}]]
-
-  def jenkins_token, do: Application.get_env(:kryten, :jenkins_token)
 
   def github_user, do: Application.get_env(:kryten, :github_user)
 
